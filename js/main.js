01cf6eb2de63527f8fe542f9e01e948a8ce023e9
marcio-piagio <75724941+marcio-piagio@users.noreply.github.com>
2023-07-09 23:53:56 -0300

    atualizando icons

--- js/main.js
+++ js/main.js
@@ -26,10 +26,10 @@
       $('.back-to-top').fadeOut('slow');
     }
   });
-  $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
-    return false;
-  });
+//   $('.back-to-top').click(function(){
+//     $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+//     return false;
+//   });
 
 	/*--/ Star ScrollTop /--*/
 	$('.scrolltop-mf').on("click", function () {
