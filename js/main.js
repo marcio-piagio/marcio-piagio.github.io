a8f3debebcc625110d7ad3c1f9012c9f5b7703d9
marcio-piagio <75724941+marcio-piagio@users.noreply.github.com>
2023-07-11 19:40:56 -0300

    bugs errata

--- js/main.js
+++ js/main.js
@@ -130,108 +130,108 @@ document.write(
 )
 
 
-var _seed = 42;
-Math.random = function() {
-  _seed = _seed * 16807 % 2147483647;
-  return (_seed - 1) / 2147483646;
-};
+// var _seed = 42;
+// Math.random = function() {
+//   _seed = _seed * 16807 % 2147483647;
+//   return (_seed - 1) / 2147483646;
+// };
 
-new Vue({
-	el: '#app',
-	components: {
-	  apexchart: VueApexCharts,
-	},
-	data: {
+// new Vue({
+// 	el: '#app',
+// 	components: {
+// 	  apexchart: VueApexCharts,
+// 	},
+// 	data: {
 	  
-	  series: [30, 30, 5, 25, 10],
-	  chartOptions: {
-		chart: {
-		  width: '100%',
-		  type: 'pie',
-		},
-		labels: ["Programação", "Análise Estatística","Business Intelligence", "Análise de Dados","Web Design"],
-		theme: {
-		  monochrome: {
-			enabled: false,
-			color: '#9c0d0d',
-			shadeTo: 'light',
-			shadeIntensity: 0.8
-		  }
-		},
-		plotOptions: {
-		  pie: {
-			dataLabels: {
-			  offset: -5
-			}
-		  }
-		},
-		title: {
-		  text: "INTERESSE PROFISSIONAL"
-		},
-		dataLabels: {
-		  formatter(val, opts) {
-			const name = opts.w.globals.labels[opts.seriesIndex]
-			return [val.toFixed(1) + '%']
-		  }
-		},
-		legend: {
-		  show: true
-		}
-	  },
+// 	  series: [30, 30, 5, 25, 10],
+// 	  chartOptions: {
+// 		chart: {
+// 		  width: '100%',
+// 		  type: 'pie',
+// 		},
+// 		labels: ["Programação", "Análise Estatística","Business Intelligence", "Análise de Dados","Web Design"],
+// 		theme: {
+// 		  monochrome: {
+// 			enabled: false,
+// 			color: '#9c0d0d',
+// 			shadeTo: 'light',
+// 			shadeIntensity: 0.8
+// 		  }
+// 		},
+// 		plotOptions: {
+// 		  pie: {
+// 			dataLabels: {
+// 			  offset: -5
+// 			}
+// 		  }
+// 		},
+// 		title: {
+// 		  text: "INTERESSE PROFISSIONAL"
+// 		},
+// 		dataLabels: {
+// 		  formatter(val, opts) {
+// 			const name = opts.w.globals.labels[opts.seriesIndex]
+// 			return [val.toFixed(1) + '%']
+// 		  }
+// 		},
+// 		legend: {
+// 		  show: true
+// 		}
+// 	  },
 	  
 	  
-	},
+// 	},
 	
-  })
+//   })
 
 
-// Data retrieved from https://worldpopulationreview.com/country-rankings/countries-by-density
-Highcharts.chart('container', {
-    chart: {
-        type: 'variablepie'
-    },
-    title: {
-        text: '',
-        align: 'left'
-    },
-    tooltip: {
-        headerFormat: '',
-        pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {point.name}</b><br/>' +
-            'Interesse: <b>{point.y} %</b><br/>' 
-			// +
-            // 'Population density (people per square km): <b>{point.z}</b><br/>'
-    },
-    series: [{
-        minPointSize: 50,
-        innerSize: '30%',
-        zMin: 10,
-        name: 'countries',
-        borderRadius: 5,
-        data: [{
-            name: 'Programação',
-            y: 30,
-            // z: 50
-        }, {
-            name: 'Analise de dados',
-            y: 30,
-            // z: 100
-        }, {
-            name: 'Análise Estatística',
-            y: 30,
-            // z: 150
-        }, {
-            name: 'Business Intelligence',
-            y: 10,
-            // z: 200
-        }],
-        colors: [
-            '#000066',
-            '#0000cc',
-            '#3333ff',
-            '#9999ff',
-            '#0ff3a0',
-            '#00e887',
-            '#23e274'
-        ]
-    }]
-});
+// // Data retrieved from https://worldpopulationreview.com/country-rankings/countries-by-density
+// Highcharts.chart('container', {
+//     chart: {
+//         type: 'variablepie'
+//     },
+//     title: {
+//         text: '',
+//         align: 'left'
+//     },
+//     tooltip: {
+//         headerFormat: '',
+//         pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {point.name}</b><br/>' +
+//             'Interesse: <b>{point.y} %</b><br/>' 
+// 			// +
+//             // 'Population density (people per square km): <b>{point.z}</b><br/>'
+//     },
+//     series: [{
+//         minPointSize: 50,
+//         innerSize: '30%',
+//         zMin: 10,
+//         name: 'countries',
+//         borderRadius: 5,
+//         data: [{
+//             name: 'Programação',
+//             y: 30,
+//             // z: 50
+//         }, {
+//             name: 'Analise de dados',
+//             y: 30,
+//             // z: 100
+//         }, {
+//             name: 'Análise Estatística',
+//             y: 30,
+//             // z: 150
+//         }, {
+//             name: 'Business Intelligence',
+//             y: 10,
+//             // z: 200
+//         }],
+//         colors: [
+//             '#000066',
+//             '#0000cc',
+//             '#3333ff',
+//             '#9999ff',
+//             '#0ff3a0',
+//             '#00e887',
+//             '#23e274'
+//         ]
+//     }]
+// });
